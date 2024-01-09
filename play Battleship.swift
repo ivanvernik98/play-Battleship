ed30921511898f88b44663d40ba7efe6c811d573
Bimbola Faminu <150906108+ivanvernik98@users.noreply.github.com>
2024-01-09 22:03:31 +0530

    Create play Battleship.swift

---
+++ play Battleship.swift
@@ -0,0 +1,8 @@
+print("Let's play Battleship!")
+print_board(board)
+
+def random_row(board):
+    return random.randint(0, len(board) - 1)
+
+def random_col(board):
+    return random.randint(0, len(board[0]) - 1)
